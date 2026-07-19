510aeb561bd2e3501eb0abae42ea3399d07e3f9e
Mohamed Nabil <mohamedmenasy@gmail.com>
2026-07-19 15:44:02 -0500

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun search(nums: IntArray, target: Int): Int {
+        var left = 0
+        var right = nums.lastIndex
+
+        while(left <= right){
+            val mid = left + (right - left) / 2
+            val num = nums[mid]
+            if(num > target){
+                right = mid - 1
+            } else if(num < target){
+                left = mid + 1
+            } else {
+                return mid
+            }
+        }
+        return -1
+    }
+}
