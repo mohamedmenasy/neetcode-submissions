bceda7253f7064f4e4e39fc66e13679d50a64137
Mohamed Nabil <mohamedmenasy@gmail.com>
2026-07-18 23:39:08 -0500

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun carFleet(target: Int, position: IntArray, speed: IntArray): Int {
+        var fleet = 0
+        var lastFleetTime = 0.0
+
+        val cars = position.indices
+                    .map { i -> position[i] to speed[i] }
+                    .sortedByDescending{ it.first }
+        for((posistion, speed) in cars){
+            val time = (target - posistion).toDouble() / speed
+            if(time > lastFleetTime){
+                fleet++
+                lastFleetTime = time
+            }
+        }
+        return fleet
+    }
+}
