9c6bb8d9fc3cb47b1f7de46892afff1d5de67d2a
Mohamed Nabil <mohamedmenasy@gmail.com>
2026-07-26 23:00:31 -0500

    Add: linked-list-cycle-detection - submission-1

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-1.kt
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun hasCycle(head: ListNode?): Boolean {
+        var fast = head
+        var slow = head
+
+        while(fast != null && fast.next != null){
+            fast = fast?.next?.next
+            slow = slow?.next
+            
+            if(fast === slow){
+                return true
+            }
+        }
+        return false
+    }
+}
