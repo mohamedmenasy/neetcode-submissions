0167a85a6a6c4dc11e03f79c6ead3ee70d1ac3b5
Mohamed Nabil <mohamedmenasy@gmail.com>
2026-08-02 01:06:07 -0500

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-3.kt
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+//O(n * m)
+class Solution {
+    fun isSubtree(root: TreeNode?, subRoot: TreeNode?): Boolean {
+        if(root == null) return false
+        if(subRoot == null) return false 
+        
+        if(isSameTree(root, subRoot)){
+            return true
+        }
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+    }
+
+    fun isSameTree(first: TreeNode?, second: TreeNode?): Boolean {
+        if(first == null && second == null) return true
+        if(first == null || second == null) return false
+        if(first.`val` != second.`val`) return false
+
+        return isSameTree(first.left, second.left) && isSameTree(second.right, second.right)
+    }
+}
